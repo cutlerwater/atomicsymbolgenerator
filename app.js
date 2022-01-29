b93ffd6ee6f9bd60522910a54000c24602220cc1
cutlerwater <cutlerwater2@live.com>
2022-01-29 11:02:38 -0500

    second commit

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-const open = document.getElementById('open');
-const close = document.getElementById('close');
-const container = document.getElementById('container');
+const from_currencyEl = document.getElementById('from_currency');
+const from_ammountEl = document.getElementById('from_ammount');
+const to_currencyEl = document.getElementById('to_currency');
+const to_ammountEl = document.getElementById('to_ammount');
+const rateEl = document.getElementById('rate');
+const exchange = document.getElementById('exchange');
 
-open.addEventListener('click', () => {
-	container.classList.add('show-nav');
+from_currencyEl.addEventListener('change', calculate);
+from_ammountEl.addEventListener('input', calculate);
+to_currencyEl.addEventListener('change', calculate);
+to_ammountEl.addEventListener('input', calculate);
+
+exchange.addEventListener('click', () => {
+	const temp = from_currencyEl.value;
+	from_currencyEl.value = to_currencyEl.value;
+	to_currencyEl.value = temp;
+	calculate();
 });
 
-close.addEventListener('click', () => {
-	container.classList.remove('show-nav');
-});
+function calculate() {
+	const from_currency = from_currencyEl.value;
+	const to_currency = to_currencyEl.value;
+	
+	fetch(`https://api.exchangerate-api.com/v4/latest/${from_currency}`)
+		.then(res => res.json())
+		.then(res => {
+		const rate = res.rates[to_currency];
+		rateEl.innerText = `1 ${from_currency} = ${rate} ${to_currency}`
+		to_ammountEl.value = (from_ammountEl.value * rate).toFixed(2);
+	})
+}
+
+calculate();
 
 // SOCIAL PANEL JS
 const floating_btn = document.querySelector('.floating-btn');
